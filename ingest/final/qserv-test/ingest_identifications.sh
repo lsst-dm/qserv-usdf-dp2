cd1ab8684e6486d768bc20b49c4234eff0dcf39a
Igor Gaponenko <gapon@slac.stanford.edu>
2026-06-26 12:34:30 -0700

    Configurations and workflows for tables current_identifications and numbered_identifications

--- ingest/final/qserv-test/ingest_identifications.sh
+++ ingest/final/qserv-test/ingest_identifications.sh
@@ -30,35 +30,16 @@ DATABASE=dp2
 DATABASE_OPT="--database=${DATABASE}"
 VERBOSE_OPT="--verbose"
 DEBUG_OPT=
-DIRECTOR_TABLES="Object Source DiaObject ShearObject DiaSourceOnSSObject IsolatedStarStellarMotions"
-PARTITIONED_TABLES="Object Source ForcedSource DiaObject DiaSource DiaSourceOnDiaObject DiaSourceOnSSObject ForcedSourceOnDiaObject ShearObject IsolatedStarStellarMotions"
-FULLY_REPLICATED_TABLES="SSObject SSSource Visit VisitDetector CoaddPatches mpc_orbits current_identifications numbered_identifications"
-ALL_TABLES="${PARTITIONED_TABLES} ${FULLY_REPLICATED_TABLES}"
+ALL_TABLES="current_identifications numbered_identifications"
 
 # CSV dialect definitions for the tables
-Object_CSV_DIALECT=
-Source_CSV_DIALECT=
-ForcedSource_CSV_DIALECT=
-DiaObject_CSV_DIALECT=
-DiaSource_CSV_DIALECT=
-DiaSourceOnDiaObject_CSV_DIALECT=
-DiaSourceOnSSObject_CSV_DIALECT=
-ForcedSourceOnDiaObject_CSV_DIALECT=
-ShearObject_CSV_DIALECT=
-SSObject_CSV_DIALECT='--fields-enclosed-by="'
-SSSource_CSV_DIALECT='--fields-enclosed-by="'
-Visit_CSV_DIALECT='--fields-enclosed-by="'
-VisitDetector_CSV_DIALECT='--fields-enclosed-by="'
-IsolatedStarStellarMotions_CSV_DIALECT=
-CoaddPatches_CSV_DIALECT='--fields-enclosed-by=" --fields-terminated-by=,'
-mpc_orbits_CSV_DIALECT="--fields-enclosed-by=' --fields-terminated-by=,"
-current_identifications_CSV_DIALECT="--fields-enclosed-by=' --fields-terminated-by=,"
-numbered_identifications_CSV_DIALECT="--fields-enclosed-by=' --fields-terminated-by=,"
+current_identifications_CSV_DIALECT="--fields-terminated-by=, --fields-enclosed-by='"
+numbered_identifications_CSV_DIALECT="--fields-terminated-by=, --fields-enclosed-by='"
 
-APP=register-database
+APP=unpublish-database
 LOG=${LOG_DIR}/${APP}.log
-echo $(TIMESTAMP)"Register database ${DATABASE} -> ${LOG}"
-${TOOLS}/${APP}.py ${DATABASE_OPT} ${VERBOSE_OPT} ${DEBUG_OPT} ../${DATABASE}.json >& ${LOG}
+echo $(TIMESTAMP)"Unpublish database ${DATABASE} -> ${LOG}"
+${TOOLS}/${APP}.py ${DATABASE_OPT} ${VERBOSE_OPT} ${DEBUG_OPT} >& ${LOG}
 if [ $? -ne 0 ] ; then
   echo $(TIMESTAMP)FAILED;
   exit 1;
@@ -75,20 +56,8 @@ for TABLE in ${ALL_TABLES}; do
   fi;
 done
 
-APP=async-contrib-chunks
-for TABLE in ${PARTITIONED_TABLES}; do
-  LOG=${LOG_DIR}/${APP}-${TABLE}.log;
-  CSV_DIALECT="${TABLE}_CSV_DIALECT";
-  echo $(TIMESTAMP)"Ingest chunk contributions into ${TABLE} -> ${LOG}";
-  ${TOOLS}/${APP}.py ${DATABASE_OPT} --table=${TABLE} ${!CSV_DIALECT} ${VERBOSE_OPT} ${DEBUG_OPT} ${DATA_DIR}/${TABLE}.urls >& ${LOG};
-  if [ $? -ne 0 ] ; then
-    echo $(TIMESTAMP)FAILED;
-    exit 1;
-  fi;
-done
-
 APP=async-contrib-table-many
-for TABLE in ${FULLY_REPLICATED_TABLES}; do
+for TABLE in ${ALL_TABLES}; do
   LOG=${LOG_DIR}/${APP}-${TABLE}.log;
   CSV_DIALECT="${TABLE}_CSV_DIALECT";
   echo $(TIMESTAMP)"Ingest table contributions into ${TABLE} -> ${LOG}";
@@ -107,17 +76,6 @@ if [ $? -ne 0 ] ; then
   echo $(TIMESTAMP)FAILED;
   exit 1;
 fi
-
-APP=create-director-index
-for TABLE in ${DIRECTOR_TABLES}; do
-  LOG=${LOG_DIR}/${APP}-${TABLE}.log;
-  echo $(TIMESTAMP)"Create director index on ${TABLE} -> ${LOG}";
-  ${TOOLS}/${APP}.py ${DATABASE_OPT} --table=${TABLE} ${VERBOSE_OPT} ${DEBUG_OPT} >& ${LOG};
-  if [ $? -ne 0 ] ; then
-    echo $(TIMESTAMP)FAILED;
-    exit 1;
-  fi;
-done
 
 APP=create-table-index
 for TABLE in ${ALL_TABLES}; do
